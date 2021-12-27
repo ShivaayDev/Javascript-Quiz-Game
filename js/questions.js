700a21c8d092cf139dd2b1181da1a5f6f64cf770
shivam123-dev <ss2249@srmist.edu.in>
2021-12-27 19:52:45 +0530

    Added overall timer and loading questions from json file

--- js/questions.js
+++ js/questions.js
@@ -1,5 +1,5 @@
 let questions = [
-    {
+  {
     numb: 1,
     question: "What does HTML stand for?",
     answer: "Hyper Text Markup Language",
@@ -10,7 +10,7 @@ let questions = [
       "Hyper Tool Multi Language"
     ]
   },
-    {
+  {
     numb: 2,
     question: "What does CSS stand for?",
     answer: "Cascading Style Sheet",
@@ -21,7 +21,7 @@ let questions = [
       "Cascading Style Sheet"
     ]
   },
-    {
+  {
     numb: 3,
     question: "What does PHP stand for?",
     answer: "Hypertext Preprocessor",
@@ -32,7 +32,7 @@ let questions = [
       "Hometext Preprocessor"
     ]
   },
-    {
+  {
     numb: 4,
     question: "What does SQL stand for?",
     answer: "Structured Query Language",
@@ -43,7 +43,7 @@ let questions = [
       "Structured Query Language"
     ]
   },
-    {
+  {
     numb: 5,
     question: "What does XML stand for?",
     answer: "eXtensible Markup Language",
